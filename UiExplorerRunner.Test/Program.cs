334f703bcb3f8ebf4ab795d0adcf501f7763731f
Maxxx <maximmahnev@gmail.com>
2025-02-24 09:48:25 +0700

    Add project files.

---
+++ UiExplorerRunner.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace UiExplorerRunner.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exeFileName = "C:\\Users\\Maxxx\\Desktop\\55555555555555\\UiExplorer.20.10.10\\runtime\\UiExplorer_x64.exe";
+            string selector = "<wnd app='explorer.exe' cls='Shell_TrayWnd' idx='*' />\u000d\u000a<wnd cls='MSTaskSwWClass' title='Работающие приложения' />\u000d\u000a<wnd cls='MSTaskListWClass' title='Работающие приложения' />\u000d\u000a<ctrl name='Работающие приложения' role='tool bar' />\u000d\u000a<ctrl automationid='Chrome' name='Google Chrome —1 запущенное окно' role='push button' idx='2' />\u000d\u000a";
+            //string selector = null;
+            string result = ExplorerRunner.Run(exeFileName, selector);
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
---
+++ UiExplorerRunner/ExplorerRunner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+namespace UiExplorerRunner
+{
+    public class ExplorerRunner
+    {
+        public static string Run(string uiExplorerExePath, string selector = null)
+        {
+            string selectorBase64 = Base64Encode(selector);
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = uiExplorerExePath,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = false,
+                StandardErrorEncoding = Encoding.GetEncoding(CultureInfo.CurrentCulture.TextInfo.OEMCodePage),
+                Arguments = selectorBase64
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+
+            string output = null;
+            while (!process.StandardOutput.EndOfStream)
+            {
+                output = process.StandardOutput.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            if (plainText == null)
+            {
+                return null;
+            }
+
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
